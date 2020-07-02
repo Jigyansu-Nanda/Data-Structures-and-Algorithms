6ed4a292a61b1ce8785826d1b48de6d0eba528e9
Jigyansu Nanda <62047889+Jigyansu-Nanda@users.noreply.github.com>
2020-07-02 22:16:02 +0530

    Update InsertionSort.java

--- 05. Sorting/01. Normal Sorting Algorithms/02. Insertion Sort/InsertionSort.java
+++ 05. Sorting/01. Normal Sorting Algorithms/02. Insertion Sort/InsertionSort.java
@@ -4,7 +4,7 @@ class InsertionSort {
 		for (int i=1; i<n; i++) {
 			int key = arr[i];
 			int j = i-1;
-			while (j >= 0 && arr[j] > key) {
+			while (j>=0 && arr[j]>key) {
 				arr[j+1] = arr[j];
 				j--;
 			}
@@ -18,8 +18,8 @@ class InsertionSort {
 		System.out.println();
 	}
 
-  public static void main(String[] args) {
-    int[] arr = {13, 32, 5, 0, 34, 67};
-    insertionSort(arr, arr.length);
-  }
+	public static void main(String[] args) {
+		int[] arr = {13, 32, 5, 0, 34, 67};
+		insertionSort(arr, arr.length);
+	}
 }
