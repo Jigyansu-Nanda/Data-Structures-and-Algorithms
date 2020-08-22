8d62d3e786d1dd43ec9331dcbfb5da07a25df1cb
Jigyansu Nanda <62047889+Jigyansu-Nanda@users.noreply.github.com>
2020-08-23 05:03:27 +0530

    Create DisjointSet.java

---
+++ 16. Disjoint Set/01. Implementation/03. union by rank & find by path compression/DisjointSet.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+class DisjointSet {
+
+	static int[] parent;
+	static int[] rank;
+
+	// initialize all values as their own parent
+	// initialize rank of all values as 0
+	static void initialize(int n) {
+		parent = new int[n];
+		rank = new int[n];
+		for (int i=0; i<n; i++) {
+			parent[i] = i;
+			rank[i] = 0;
+		}
+	}
+
+	static int find(int x) {
+		// if element is it's own representative,
+		// return it's value
+		if (parent[x] == x) {
+			return x;
+		}
+		// set representative of parent
+		// as it's parent
+		// traversal path length might get compressed by 1
+		parent[x] = find(parent[x]);
+		// return parent
+		return parent[x];
+	}
+
+	static void union(int x, int y) {
+		int rep_of_x = find(x);
+		int rep_of_y = find(y);
+		// if already in same subset, return
+		if (rep_of_x == rep_of_y) {return ;}
+		// take the larger height tree, and
+		// make it parent of smaller height tree
+		if (rank[rep_of_x] < rank[rep_of_y]) {
+			parent[rep_of_x] = rep_of_y;
+		}
+		if (rank[rep_of_y] < rank[rep_of_x]) {
+			parent[rep_of_y] = rep_of_x;
+		}
+		// if both have equal rank,
+		// choose parent of one to be another,
+		// increase rank of parent
+		else {
+			parent[rep_of_x] = rep_of_y;
+			rank[rep_of_y] += 1;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		// driver code
+		initialize(5);
+		union(0, 3);
+		union(1, 4);
+		System.out.println(find(1) == find(4));
+		System.out.println(find(2));
+	}
+}
