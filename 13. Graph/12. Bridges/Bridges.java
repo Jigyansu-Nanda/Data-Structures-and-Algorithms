49ad86a39b8e9fb0512f49dfea5a8c5ac0ec3620
Jigyansu Nanda <62047889+Jigyansu-Nanda@users.noreply.github.com>
2020-10-15 14:10:59 +0530

    Update Bridges.java

--- 13. Graph/12. Bridges/Bridges.java
+++ 13. Graph/12. Bridges/Bridges.java
@@ -1,69 +1,70 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
 
 class Graph {
 
-	int v;
-	ArrayList<ArrayList<Integer>> graph;
-	int time = 0;
+    int n;
+    ArrayList<ArrayList<Integer>> graph;
+    static int time = 0;
 
-	public Graph (int v) {
-		this.v = v;
-		this.graph = new ArrayList<ArrayList<Integer>>();
-		for (int i=0; i<v; i++) {
-			graph.add(new ArrayList<Integer>());
-		}
-	}
+    Graph (int n) {
+        this.n = n;
+        this.graph = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+    }
 
-	void addEdge (int src, int dst) {
-		graph.get(src).add(dst);
-		graph.get(dst).add(src);
-	}
+    void addEdge (int a, int b) {
+        graph.get(a).add(b);
+        graph.get(b).add(a);
+    }
 
-	void findBridges () {
-		boolean[] vis = new boolean[v];
-		int[] parent = new int[v];
-		Arrays.fill(parent, -1);
-		int[] low = new int[v];
-		int[] disc = new int[v];
-		for (int i=0; i<v; i++) {
-			if (vis[i] == false) {
-				dfs(i, vis, parent, low, disc);
-			}
-		}
-	}
+    void bridges () {
+        int[] parent = new int[n];
+        Arrays.fill(parent, -1);
+        int[] disc = new int[n];
+        Arrays.fill(disc, -1);
+        int[] low = new int[n];
+        for (int i=0; i<n; i++) {
+            if (disc[i] == -1) {
+                dfs(i, disc, low, parent);
+            }
+        }
+    }
 
-	void dfs (int src, boolean[] vis, int[] parent, int[] low, int[] disc) {
-		vis[src] = true; 
-		disc[src] = low[src] = ++time;
-		int children = 0;
-		for (int adj: graph.get(src)) {
-			if (!vis[adj]) {
-				children++;
-				parent[adj] = src;
-				dfs(adj, vis, parent, low, disc);
-				low[src] = Math.min(low[src], low[adj]);
-				if (low[adj] > disc[src]) {
-					System.out.println(src+" "+adj);
-				}
-			}
-			else if (parent[src] != adj) {
-				low[src] = Math.min(low[src], disc[adj]);
-			}
-		}
-	}
+
+    void dfs (int src, int[] disc, int[] low, int[] parent) {
+        disc[src] = low[src] = ++time;
+        for (int child: graph.get(src)) {
+            if (disc[child] == -1) {
+                parent[child] = src;
+                dfs(child, disc, low, parent);
+                low[src] = Math.min(low[src], low[child]);
+                if (low[child] > disc[src]) {
+                    System.out.println(src+" "+child);
+                }
+            }
+            else if (parent[src] != child) {
+                low[src] = Math.min(low[src], disc[child]);
+            }
+        }
+    }
 
 }
 
-public class Bridges {
 
-	public static void main (String[] args) throws IOException {
-		Graph graph = new Graph(5);
-		graph.addEdge(0, 1);
-		graph.addEdge(1, 2);
-		graph.addEdge(0, 2);
-		graph.addEdge(0, 3);
-		graph.addEdge(3, 4);
-		graph.findBridges();
-	}
+class Bridges {
+	
+    public static void main(String[] args) throws IOException {
+    	Graph graph = new Graph(5);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(3, 4);
+        graph.bridges();
+    }
 }
